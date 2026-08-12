28a09959029018139351204cad2810a670ee8c92
Haileab <womahaileab1428@gmail.com>
2026-08-12 11:42:06 +0300

    setting up the generate boilerplate

--- app.js
+++ app.js
@@ -1,6 +1,43 @@
 
-const express = require("express");
-const app = express();
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+const passport = require('passport');
+
+/**
+ * -------------- GENERAL SETUP ----------------
+ */
+
+// Gives us access to variables set in the .env file via `process.env.VARIABLE_NAME` syntax
+require('dotenv').config();
+
+// Create the Express application
+const  app = express();
+
+
+
+// Pass the global passport object into the configuration function
+//require('./config/passport');
+
+// This will initialize the passport object on every request
+app.use(passport.initialize());
+
+// Instead of using body-parser middleware, use the new Express implementation of the same thing
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+// Allows our Angular application to make HTTP requests to Express application
+app.use(cors());
+
+// Where Angular builds to - In the ./angular/angular.json file, you will find this configuration
+// at the property: projects.angular.architect.build.options.outputPath
+// When you run `ng build`, the output will go to the ./public directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+/**
+ * -------------- ROUTES ----------------
+ */
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, (error) => {
