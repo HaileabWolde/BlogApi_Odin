32d2cc82f3d58edc6dd93dcff4dcb8165ae1847f
Haileab <womahaileab1428@gmail.com>
2026-08-19 18:14:37 +0300

    deleted every data

--- backend/script.js
+++ backend/script.js
@@ -2,7 +2,11 @@ const prisma = require('./lib/prisma');
 
 async function main() {
   //await prisma.post.deleteMany()
+  await prisma.postTag.deleteMany()
+   await prisma.post.deleteMany()
    await prisma.user.deleteMany()
+   
+   await prisma.tag.deleteMany()
   // Create a new user with a post
   /*
   const user = await prisma.user.create({
